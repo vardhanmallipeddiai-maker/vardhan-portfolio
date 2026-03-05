b1120226f7096ae9e2241e2814f7aaf6a7d45f15
vardhanmallipeddiai-maker <vardhanmallipeddi.ai@gmail.com>
2026-03-05 15:42:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-    // Get the contact button
     const contactButton = document.getElementById('contactBtn');
 
-    // When button is clicked
     contactButton.addEventListener('click', function() {
-
-        // Open user's email application
-        window.location.href = "mailto:mallipeddivardhan9@gmail.com";
-
+        window.open(
+            "https://mail.google.com/mail/?view=cm&fs=1&to=mallipeddivardhan9@gmail.com",
+            "_blank"
+        );
     });
-
 });
